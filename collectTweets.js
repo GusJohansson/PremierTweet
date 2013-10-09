0a8844edb5ea4a84999abeb792fde1810cf2cd78
GusJohansson <g89.johansson@gmail.com>
2013-10-09 15:21:26 +0200

    keys and shit

---
+++ collectTweets.js
@@ -0,0 +1,17 @@
+var twitterAPI = require('twitter');
+var twitter = new twitterAPI({
+	consumer_key: "U9RL4b02ZiIBUr78DE7gA", 
+	consumer_secret: "hqpjfYrUeW1rT5Qc24qSFdtVgFgOWJvwhT06tL32P4",
+	access_token_key: "1430661354-cI9ayb1EgA47Vftv2o1aVNBfG3OphESHh1Qjyjl", 
+	access_token_secret: "N014Hhl3wMA2d3fv9stIy8oyYilk2jvrP7pGvN13gBCIi"
+});
+
+twitter.stream("user", { track: ['anonymous']}, function(stream) {
+	stream.on('data', function(data) {
+		if (data.user) {
+			console.log (new
+				Date(data.created_at).getTime() + "; " + data.user['name'] + ": " +
+				data.text);
+		}
+	});
+});
